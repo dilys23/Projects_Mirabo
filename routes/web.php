d7c767700cb8cf8c4cf1032a7749778f6a45a322
Dilysnguyen <91045422+dilys23@users.noreply.github.com>
2024-06-20 14:57:01 +0700

    create project

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users', function () {
+    return "welcome to my website";
+});
+
+Route::get('/array', function()
+{
+    return ['xoai', 'cam', 'tofu'];
+});
+Route::get('/json', function()
+{
+    return response() -> json ([
+        'name' => 'Nguyen Thi Le',
+         'email' => 'lenguyen@gmail.com'
+    ]);
+});
